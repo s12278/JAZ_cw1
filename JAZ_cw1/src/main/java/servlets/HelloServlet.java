7f29661565f2fc1cff921d5d96db373a865c00ce
s12278 <prstokarski@gmail.com>
2016-03-05 18:59:20 +0100

    Koniec na dziś

--- JAZ_cw1/src/main/java/servlets/HelloServlet.java
+++ JAZ_cw1/src/main/java/servlets/HelloServlet.java
@@ -17,8 +17,9 @@ public class HelloServlet extends HttpServlet {
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException
 	{
+		String name = request.getParameter("name");
 		response.setContentType("text/html");
-		response.getWriter().println("<h1>Hello world, hey!</h1>");
+		response.getWriter().println("<h1>Hello world, hey " + name + "</h1>");
 		
 	}
 	
